69461c86da6f439b47c25b6fd5214328fa9422b5
tradestimate-cpu <tradestimate@gmail.com>
2026-01-04 18:33:35 +0530

    Fix missing dependencies for Render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // MONGODB CONNECTION
 mongoose.connect(
-  "mongodb+srv://tradEstimate:Tradestimate1802@cluster0.h3sdezg.mongodb.net/tradEstimateDB"
+  "mongodb+srv://trdEstimate:Tradestimate1802@cluster0.h3sdezg.mongodb.net/tradEstimateDB"
 )
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log("MongoDB Error:", err.message));
@@ -42,6 +42,37 @@ app.post("/contact", async (req, res) => {
     res.status(500).json({ message: "Error saving data" });
   }
 });
+
+app.get("/test-db", async (req, res) => {
+  await Contact.create({
+    name: "Test User",
+    email: "test@test.com",
+    mobile: "9999999999",
+    message: "Test entry from API"
+  });
+  res.send("Test data inserted");
+});
+app.get("/__debug_insert__", async (req, res) => {
+  try {
+    const doc = await Contact.create({
+      name: "DEBUG USER",
+      email: "debug@test.com",
+      mobile: "1111111111",
+      message: "This is a forced debug insert"
+    });
+
+    res.json({
+      success: true,
+      id: doc._id
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
+});
+
 
 // SERVER START
 app.listen(5000, () => {
